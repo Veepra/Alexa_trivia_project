87118f16372263d9a6496ab3e983516a5902b3d0
veepra <veeralakshmi@gmail.com>
2018-02-07 18:54:48 -0500

    baby trivia questions

--- lambda/custom/question.js
+++ lambda/custom/question.js
@@ -256,23 +256,258 @@ module.exports = {
     ],
     QUESTIONS_EN_US: [
         {
-            'Babies recognize its mother\'s voice at birth, how long does it take a baby to recognize its father\'s voice?': [
-                '14 days',
-                '7 days',
-                '5 days',
-                '30 days',
+            'How many is the most surviving children from a single birth?': [
+                '8',
+                '3',
+                '5',
+                '7',
             ],
         },
         {
-            'How many is the most surviving children from a single birth?': [
-                '8 children',
-                '10 children',
-                '5 children',
-                '15 children',
+            'Babies recognize its mother\'s voice at birth, how long does it take a baby to recognize its father\'s voice?': [
+                '14 days',
+                '12 days',
+                '1 day',
+                'as soon as the baby is born',
+            ],
+        },
+        {
+            'When does a baby get its fingerprints?': [
+            	'after it is three months old ',
+                'shortly after birth',
+                'as a three month fetus',
+                'as a six month fetus',
 
             ],
         },
-        
+        {
+            'In Turkish Tradition, a pregnant woman is not allowed to do any of the following except...?': [
+                'Look at beautiful people',
+                'Look at a monkey',
+                'Chew Gum',
+                'Attend a funeral',
+
+
+            ],
+        },
+        {
+            'How many average diapers will a child go through from birth to being potty trained?': [
+                '7,000-9,000 diapers',
+                '12,000-15,000 diapers',
+                '20,000 diapers',
+                '2000 diapers',
+
+            ],
+        },
+        {
+            'In England, a pacifier is also known as…': [
+
+                'a dummy',
+                'a binky',
+                'a sucky',
+                'a mouthy thingy thing',
+
+            ],
+        },
+        {
+            'In Jewish Tradition, what cannot be done before a baby is born?': [
+                'Buy baby gifts',
+                'Choose a baby name',
+                'Celebrate Hanukkah',
+                'Celebrate new year',
+
+            ],
+        },
+        {
+            'Which of the following is not a natural born reflex of a baby?': [
+                'Sucking',
+                'Fanning toes out when touched',
+                'Flinging out arms as a jolt',
+                'Rolling Over',
+
+            ],
+        },
+        {
+            'How many years does it take a diaper to decompose?': [
+                '15 years',
+                '120 years',
+                '500 years',
+                '700 years',
+
+            ],
+        },
+        {
+            '. What is the average amount of diapers a newborn goes through in one day?': [
+                '8-12',
+                '20-25',
+                '3-6',
+                '30-60',
+
+            ],
+        },
+        {
+            'The old wives tale birth prediction states you should do what with a wedding ring?': [
+            	'Suspend it on a string over the pregnant woman\'s hand. The direction it swings will determine the gender.',
+                'Put it under the mattress before bringing the pregnant woman into the room- the side of the bed the pregnant woman sits on will determine the gender.',
+                'Put it on cheesecloth and pour goat\'s milk through it into a glass. The milk will taste sweet if it is a girl and bitter if it is a boy.',
+                'no such thing exists',
+            ],
+        },
+        {
+            '. What can a baby do up until 7 months that an adult cannot?': [
+                'Breathe and swallow at the same time',
+                'Cross its eyes while sticking out its tongue',
+                'Curl up into a ball and suck its thumb',
+                'cry and laugh',
+               ],
+        },
+        {
+            'How many other people does the average child share his/her birthday with?': [
+                '9 Million',
+                '300,000',
+                '225 Million',
+                '1 Billion',
+
+            ],
+        },
+        {
+            'What is a diaper called in South Africa?': [
+                'Disposable',
+                'Nappy',
+                'Dispenser',
+               'pad',
+            ],
+        },
+        {
+            'During pregnancy how much larger does a woman\'s uterus stretch?': [
+                '40 times its normal size',
+                '200 times its normal size',
+                '500 times its normal size',
+                '100 times its normal size',
+
+            ],
+        },
+        {
+            'In England, when a person says they will "knock you up in the morning" it means:': [
+                'You will get pregnant in the morning.',
+                'They will knock on your door in the morning.',
+                'You will get beat up in the morning',
+                'knock on your head',
+
+            ],
+        },
+        {
+            'A newborn should be given a sponge bath until what happens?': [
+                'The umbilical cord falls off.',
+                'The baby likes taking baths',
+                'He/She turns 6 months and can sit up properly',
+                'he/she walks',
+
+            ],
+        },
+        {
+            ' A baby is born around the world...?': [
+                'Every three seconds',
+                'Every twenty seconds',
+                'Every minute',
+                'Every 20 seconds',
+                '1,000',
+                '120,000',
+            ],
+        },
+        {
+            '. A baby should be seated in a rear child car seat until?': [
+                'the baby turns 2 and has reached the weight limit for the rear facing car seat',
+                'the baby turns one and weighs at least 20 pounds',
+                'the baby weighs 15-20 pounds and is at least 36 inches tall',
+                'the baby weighs 45-50 pounds and is at least 36 inches tall',
+            ],
+        },
+        {
+            'The average toddler will take how many steps a minute?': [
+                '176 steps',
+                '550 steps',
+                '75 steps',
+                '220 steps',
+
+            ],
+        },
+        {
+            'Newborns can see best at a distance of:?': [
+                '8 to 14 inches',
+                '2 to 6 inches',
+                '2 to 3 feet',
+                '2 to 7 feet',
+            ],
+        },
+        {
+            'Babies are born with survival reflexes. true or false	?': [
+                'True',
+                'False',
+                'neither true nor false',
+                'true and false',
+
+            ],
+        },
+        {
+            'A newborn starts gaining weight rapidly during her first day of life. true or false?': [
+                 'False',
+                 'True',
+               'neither true nor false',
+                'true and false',
+            ],
+        },
+        {
+            'True or false: It is normal for a newborn to have irregular breathing patterns.?': [
+                'True',
+                'False',
+                'neither true nor false',
+                'true and false',
+            ],
+        },
+        {
+            'True or false: The top layer of a newborn\'s skin will peel off.?': [
+                 'True',
+                'False',
+                'neither true nor false',
+                'true and false',
+            ],
+        },
+        {
+            'True or false: A newborn\'s eye color at birth is permanent.?': [
+                'False',
+                 'True',
+                 'neither true nor false',
+                'true and false',
+            ],
+        },
+        {
+            'True or false: Many newborns recognize songs and voices that they heard before they were born.?': [
+                  'True',
+                'False',
+                'neither true nor false',
+                'true and false',
+            ],
+        },
+        {
+            'True or false: A newborn goes through the same sleep cycles as an adult.?': [
+               'True',
+                'False',
+                'neither true nor false',
+                'true and false',
+            ],
+        },
+        {
+            'What is the  Average time for a man to change diaper?': [
+                '1 minute, 36 seconds.',
+                '1 minute.',
+                '5 minute',
+                '30 seconds',
+
+            ],
+        },
+
+
     ],
     QUESTIONS_DE_DE: [
         {
